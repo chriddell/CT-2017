b2477672361ed0c12c995157c3557787a2cf7860
Chris Liddell <chrisliddell1991@gmail.com>
2017-03-20 11:28:13 +0000

    Site Footer styles.

--- footer.php
+++ footer.php
@@ -11,9 +11,15 @@
  * @since 1.0
  * @version 1.0
  */
-
-wp_footer(); ?>
+?>
 
 </div><!-- #main-content -->
+<footer class="l-footer c-site-footer" id="footer" role="footer">
+	<div class="l-site-wrapper">
+		<p class="c-site-footer__legal u-float-left">SS&amp;C Advent &copy; <?php the_date('Y'); ?></p>
+		<span class="c-site-logo c-site-logo--white c-site-footer__logo u-float-right"></span>
+	</div>
+</footer>
+<?php wp_footer(); ?>
 </body>
 </html>
