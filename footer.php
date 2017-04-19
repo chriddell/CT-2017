e5083584270503234a9ac17bf51ea5cb40c89d86
Chris Liddell <chrisliddell1991@gmail.com>
2017-04-19 16:05:36 +0100

    https://trello.com/c/fwyiILdH/50-update-footer-as-per-v4-design

--- footer.php
+++ footer.php
@@ -18,11 +18,11 @@
 		<div class="l-wrapper u-clearfix">
 			<a class="c-block-link c-dual-footer__link c-read-more__parent l-col-12 l-col-sml-4 l-col-med-4 u-no-hover" href="http://investmentmanagement.tech/">
 				<h4 class="c-block-link__title c-dual-footer__link-title c-read-more">Investment Management</h4>
-				<p class="c-block-link__copy c-dual-footer__link-copy">Where you can learn more about our products and services</p>
+				<p class="c-block-link__copy c-dual-footer__link-copy">Our solutions and services</p>
 			</a>
 			<a class="c-block-link c-dual-footer__link c-read-more__parent l-col-12 l-col-sml-4 l-col-med-4 u-no-hover" href="<?php echo site_url('/'); ?>">
 				<h4 class="c-block-link__title c-dual-footer__link-title c-read-more">Capital Trends</h4>
-				<p class="c-block-link__copy c-dual-footer__link-copy">Where our people share their insights, views and expertise.</p>
+				<p class="c-block-link__copy c-dual-footer__link-copy">Industry insights</p>
 			</a>
 			<span class="l-col-12 l-col-sml-4-last l-col-med-3-last">
 				<span class="c-site-logo c-site-logo--white c-site-footer__logo u-float-right"></span>
@@ -31,7 +31,7 @@
 	</div>
 	<div class="c-site-footer__section c-legal-footer">
 		<div class="l-wrapper">
-			<p class="c-legal-footer__copy u-float-right">SS&amp;C Advent &copy; <?php echo get_the_date('Y'); ?></p>
+			<p class="c-legal-footer__copy">SS&amp;C Advent &copy; <?php echo get_the_date('Y'); ?></p>
 		</div>
 	</div>
 </footer>
