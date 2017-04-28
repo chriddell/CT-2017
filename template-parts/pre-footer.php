837a5dc586ea364c9e0368da8b04491b1df3ed5c
Chris Liddell <chrisliddell1991@gmail.com>
2017-04-28 11:21:35 +0100

    Reduce size of hero.

--- template-parts/pre-footer.php
+++ template-parts/pre-footer.php
@@ -12,7 +12,7 @@
 <aside class="l-pre-footer c-pre-footer" id="pre-footer">
 	<span class="l-wrapper">
 		<span class="l-col-12 l-col-sml-10 l-col-sml-push-1 l-col-med-8 l-col-med-push-2">
-			<h2 class="c-pre-footer__title u-heading">Contact us to request a demo and find out more about how we can help you.</h2>
+			<h2 class="c-pre-footer__title u-heading">Contact us to find out more about how we can help you.</h2>
 			<a href="#contact-form" class="c-btn c-btn--white c-btn--centered c-read-more" data-rel="lightcase" type="inline">Get a demo</a>
 		</span>
 	</span>
