39cbe8ca1c38602a5ac4e42b1f2e4b15cc58ae4f
Chris Liddell <chrisliddell1991@gmail.com>
2017-05-03 10:06:48 +0100

    Change copy on footer 'contact' button

--- template-parts/pre-footer.php
+++ template-parts/pre-footer.php
@@ -13,7 +13,7 @@
 	<span class="l-wrapper">
 		<span class="l-col-12 l-col-sml-10 l-col-sml-push-1 l-col-med-8 l-col-med-push-2">
 			<h2 class="c-pre-footer__title u-heading">Contact us to find out more about how we can help you.</h2>
-			<a href="#contact-form" class="c-btn c-btn--white c-btn--centered c-read-more" data-rel="lightcase" type="inline">Get a demo</a>
+			<a href="#contact-form" class="c-btn c-btn--white c-btn--centered c-read-more" data-rel="lightcase" type="inline">Learn more</a>
 		</span>
 	</span>
 
