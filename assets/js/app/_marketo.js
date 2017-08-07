de58846a54e820eddef229c89cb7f61c451510a8
Chris Liddell <chrisliddell1991@gmail.com>
2017-08-07 17:13:52 +0100

    https://trello.com/c/rUCSzQvX/79-unhide-labels-on-marketo-forms

--- assets/js/app/_marketo.js
+++ assets/js/app/_marketo.js
@@ -38,10 +38,10 @@
 		$( '.mktoForm style' ).remove();
 
 		// Remove classes & styles from individual elements within <form>
-		$( '.mktoForm input, .mktoForm textarea, .mktoForm label, .mktoForm .mktoFormCol, .mktoForm .mktoButtonWrap, .mktoRequired' ).removeAttr( 'style' );
+		$( '.mktoForm input, .mktoForm textarea, .mktoForm label, .mktoForm .mktoFormCol, .mktoForm .mktoButtonWrap, .mktoRequired, .mktoCheckboxList' ).removeAttr( 'style' );
 		$( '.mktoForm' ).css( 'width', '100%' );
 		$( '.mktoOffset, .mktoGutter' ).remove();
-		$( 'label' ).remove();
+		$( '.mktoAsterix' ).remove(); // this essentially hides unused <labels>
 		$( 'input.mktoField, input.mktoTextField' ).removeClass( 'mktoField' ).removeClass( 'mktoTextField' );
 		$( '.mktoFieldWrap' ).removeClass( 'mktoFieldWrap' );
 		$( '.mktoFormCol' ).removeClass( 'mktoFormCol' );
@@ -50,14 +50,17 @@
 		$( '.mktoForm button' ).addClass( 'register' ).removeClass( 'mktoButton' );
 
 		// Remove classes and style attr from <form> element
-		$( '.mktoForm' ).removeAttr( 'style' ).removeClass('mktoForm mktoHasWidth mktoLayoutAbove');
+		$( '.mktoForm' ).removeAttr( 'style' ).removeClass( 'mktoForm mktoHasWidth mktoLayoutAbove' );
 
 		// Add our own classes :)
-	  $('.c-form--marketo input, .c-form--marketo button, .c-form--marketo select, .c-form--marketo textarea').addClass('c-form__input');
-	  $('.c-form--marketo input[type="text"], .c-form--marketo input[type="email"], .c-form--marketo textarea').addClass('c-form__input--text');
-	  $('.c-form--marketo textarea').addClass('c-form__input--textarea');
-	  $('.c-form--marketo button[type="submit"]').addClass('c-form__input c-form__input--submit c-btn c-btn--submit c-read-more');
-	  $('.c-form--marketo select').addClass('c-form__input--select').parents( '.mktoFormRow' ).addClass('c-form__input-group--50');;
+	  $('.c-form--marketo input, .c-form--marketo button, .c-form--marketo select, .c-form--marketo textarea').addClass( 'c-form__input' );
+	  $('.c-form--marketo input[type="text"], .c-form--marketo input[type="email"], .c-form--marketo textarea').addClass( 'c-form__input--text' );
+	  $('.c-form--marketo input[type="checkbox"]').addClass( 'c-form__input--checkbox' );
+	  $( '.c-form--marketo textarea' ).addClass( 'c-form__input--textarea' );
+	  $( '.c-form--marketo button[type="submit"]' ).addClass( 'c-form__input c-form__input--submit c-btn c-btn--submit c-read-more' );
+	  $( '.c-form--marketo select' ).addClass( 'c-form__input--select' ).parents( '.mktoFormRow' ).addClass( 'c-form__input-group--50' );
+	  $( '.c-form--marketo label' ).addClass( 'c-form__label' );
+	  $( '.c-form--marketo input[type="checkbox"] + label' ).addClass( 'c-form__label--checkbox' );
 	}
 
 })(jQuery);
